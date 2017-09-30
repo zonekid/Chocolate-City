1cd5b73ba27bc7cf1e4a16f4133091f42957d5d3
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-09-30 13:06:16 -0400

    fixes

--- public/js/app.js
+++ public/js/app.js
@@ -2,88 +2,65 @@
         "use strict";
         angular
             .module("chocolate", ["ui.router"])
-            .config(["$stateProvider", RouterFunction])
+            .config(["$stateProvider", function($stateProvider) {
+                $stateProvider
+                    .state("chocolateMain", {
+                        url: "/main",
+                        templateUrl: "public/js/ng-views/main.html",
+                        controller: "ChocolateMainController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateDirector", {
+                        url: "/director",
+                        templateUrl: "public/js/ng-views/director.html",
+                        controller: "ChocolateDirectorController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateCharacter", {
+                        url: "/character",
+                        templateUrl: "public/js/ng-views/character.html",
+                        controller: "ChocolateCharacterController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateContact", {
+                        url: "/contact",
+                        templateUrl: "public/js/ng-views/contact.html",
+                        controller: "ChocolateContactController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolatePartners", {
+                        url: "/partners",
+                        templateUrl: "public/js/ng-views/partners.html",
+                        controller: "ChocolatePartnersController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateTrailer", {
+                        url: "/trailer",
+                        templateUrl: "public/js/ng-views/trailer.html",
+                        controller: "ChocolateTrailerController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateGallery", {
+                        url: "/gallery",
+                        templateUrl: "public/js/ng-views/gallery.html",
+                        controller: "ChocolateGalleryController",
+                        controllerAs: "vm"
+                    })
+                    .state("chocolateAction", {
+                        url: "/action",
+                        templateUrl: "public/js/ng-views/action.html",
+                        controller: "ChocolateActionController",
+                        controllerAs: "vm"
+                    })
 
-            .controller("ChocolateMainController", [
-                ChocolateMainControllerFunction
-            ])
-
-            .controller("ChocolateDirectorController", [
-                ChocolateDirectorControllerFunction
-            ])
-            .controller("ChocolateCharacterController", [
-                ChocolateCharacterControllerFunction
-            ])
-            .controller("ChocolateContactController", [
-                ChocolateContactControllerFunction
-            ])
-            .controller("ChocolatePartnersController", [
-                ChocolatePartnersControllerFunction
-            ])
-            .controller("ChocolateTrailerController", [
-                ChocolateTrailerControllerFunction
-            ])
-            .controller("ChocolateGalleryController", [
-                ChocolateGalleryControllerFunction
-            ])
-            .controller("ChocolateActionController", [
-                ChocolateActionControllerFunction
-            ])
-
-
-
-
-        function RouterFunction($stateProvider) {
-            $stateProvider
-                .state("chocolateMain", {
-                    url: "/main",
-                    templateUrl: "public/js/ng-views/main.html",
-                    controller: "ChocolateMainController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateDirector", {
-                    url: "/director",
-                    templateUrl: "public/js/ng-views/director.html",
-                    controller: "ChocolateDirectorController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateCharacter", {
-                    url: "/character",
-                    templateUrl: "public/js/ng-views/character.html",
-                    controller: "ChocolateCharacterController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateContact", {
-                    url: "/contact",
-                    templateUrl: "public/js/ng-views/contact.html",
-                    controller: "ChocolateContactController",
-                    controllerAs: "vm"
-                })
-                .state("chocolatePartners", {
-                    url: "/partners",
-                    templateUrl: "public/js/ng-views/partners.html",
-                    controller: "ChocolatePartnersController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateTrailer", {
-                    url: "/trailer",
-                    templateUrl: "public/js/ng-views/trailer.html",
-                    controller: "ChocolateTrailerController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateGallery", {
-                    url: "/gallery",
-                    templateUrl: "public/js/ng-views/gallery.html",
-                    controller: "ChocolateGalleryController",
-                    controllerAs: "vm"
-                })
-                .state("chocolateAction", {
-                    url: "/action",
-                    templateUrl: "public/js/ng-views/action.html",
-                    controller: "ChocolateActionController",
-                    controllerAs: "vm"
-                })
-
-        }
+            }])
+            .controller("ChocolateMainController", [function(){}])
+            .controller("ChocolateDirectorController", [function(){}])
+            .controller("ChocolateCharacterController", [function(){}])
+            .controller("ChocolateContactController", [function(){}])
+            .controller("ChocolatePartnersController", [function(){}])
+            .controller("ChocolateTrailerController", [function(){}])
+            .controller("ChocolateGalleryController", [function(){}])
+            .controller("ChocolateActionController", [function(){}])
     }
     ())
